5b1f04352a78387fe5de7bd143cec88325223798
unsubstantiated-Script <jusfulton@gmail.com>
2022-02-22 08:37:52 -0800

    done

--- skripts.js
+++ skripts.js
@@ -1,1 +1,23 @@
 console.log("howdy!");
+
+//Local Storage
+localStorage.setItem("name", "Bobz");
+//localStorage.removeItem("name")
+console.log(localStorage.getItem("name"));
+
+//Session Storage
+sessionStorage.setItem("name", "Sesshy");
+//sessionStorage.removeItem("name")
+console.log(sessionStorage.getItem("name"));
+
+//Cookies aren't fun...
+
+document.cookie =
+	"name=CookieMonster; expires=" + new Date(2022, 02, 25).toUTCString();
+
+//Adding a new cookie is as simple as
+document.cookie =
+	"otherName=Grover; expires=" + new Date(2022, 02, 25).toUTCString();
+
+//View
+console.log(document.cookie);
